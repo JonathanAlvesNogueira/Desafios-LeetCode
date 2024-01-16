8a44a9659ed2641879be02b4ca9606f60811419d
Jonathan <joth10@live.com>
2024-01-15 23:31:23 -0300

    Adicionando dois exercicios de sql

---
+++ exercicio2.sql
@@ -0,0 +1,53 @@
+/*
+
+Mesa: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+In SQL, id is the primary key column for this table.
+Each row of this table indicates the id of a customer, their name, and the id of the customer who referred them.
+ 
+
+Encontre os nomes do cliente que não são indicados pelo cliente com o .id = 2
+
+Retorne a tabela de resultados em qualquer ordem.
+
+O formato do resultado está no exemplo a seguir.
+
+ 
+
+Exemplo 1:
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+
+
+*/
+
+-- resolução
+# Write your MySQL query statement below
+SELECT NAME FROM CUSTOMER WHERE REFEREE_ID != 2 OR REFEREE_ID IS NULL;
