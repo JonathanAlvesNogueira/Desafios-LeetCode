d1eabfc0173c5d3a81b3ebeef610fa45f5c80d07
Jonathan <joth10@live.com>
2024-01-16 01:06:48 -0300

    Adicionando exercicio 3

---
+++ exercicio3.sql
@@ -0,0 +1,57 @@
+/*
+Mesa: World
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+name is the primary key (column with unique values) for this table.
+Each row of this table gives information about the name of a country, the continent to which it belongs, its area, the population, and its GDP value.
+ 
+
+Um país é grande se:
+
+tem uma área de pelo menos três milhões (ou seja, ), ou3000000 km2
+Tem uma população de pelo menos vinte e cinco milhões (i.e., ).25000000
+Escreva uma solução para encontrar o nome, a população e a área dos grandes países.
+
+Retorne a tabela de resultados em qualquer ordem.
+
+O formato do resultado está no exemplo a seguir.
+
+ 
+
+Exemplo 1:
+
+Input: 
+World table:
++-------------+-----------+---------+------------+--------------+
+| name        | continent | area    | population | gdp          |
++-------------+-----------+---------+------------+--------------+
+| Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+| Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+| Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+| Andorra     | Europe    | 468     | 78115      | 3712000000   |
+| Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
++-------------+-----------+---------+------------+--------------+
+Output: 
++-------------+------------+---------+
+| name        | population | area    |
++-------------+------------+---------+
+| Afghanistan | 25500100   | 652230  |
+| Algeria     | 37100000   | 2381741 |
++-------------+------------+---------+
+
+*/
+
+SELECT NAME
+       ,POPULATION
+       ,AREA
+FROM WORLD 
+WHERE POPULATION >= 25000000 OR AREA >= 3000000
+;
