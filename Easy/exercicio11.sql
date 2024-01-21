7bd2a67d5b18498923f88f0ab861577cfe4b57aa
Jonathan <joth10@live.com>
2024-01-20 22:27:13 -0300

    Adicionando exercicio 11 facil

---
+++ Easy/exercicio11.sql
@@ -0,0 +1,70 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| empId       | int     |
+| name        | varchar |
+| supervisor  | int     |
+| salary      | int     |
++-------------+---------+
+empId is the column with unique values for this table.
+Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+ 
+
+Mesa: Bonus
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| empId       | int  |
+| bonus       | int  |
++-------------+------+
+empId is the column of unique values for this table.
+empId is a foreign key (reference column) to empId from the Employee table.
+Each row of this table contains the id of an employee and their respective bonus.
+ 
+
+Escreva uma solução para informar o nome e o valor do bônus de cada funcionário com um bônus menor que .1000
+
+Retorne a tabela de resultados em qualquer ordem.
+
+O formato do resultado está no exemplo a seguir.
+
+ 
+
+Exemplo 1:
+
+Input: 
+Employee table:
++-------+--------+------------+--------+
+| empId | name   | supervisor | salary |
++-------+--------+------------+--------+
+| 3     | Brad   | null       | 4000   |
+| 1     | John   | 3          | 1000   |
+| 2     | Dan    | 3          | 2000   |
+| 4     | Thomas | 3          | 4000   |
++-------+--------+------------+--------+
+Bonus table:
++-------+-------+
+| empId | bonus |
++-------+-------+
+| 2     | 500   |
+| 4     | 2000  |
++-------+-------+
+Output: 
++------+-------+
+| name | bonus |
++------+-------+
+| Brad | null  |
+| John | null  |
+| Dan  | 500   |
++------+-------+
+*/
+
+SELECT  
+    A.NAME
+    ,B.BONUS
+FROM EMPLOYEE A
+LEFT JOIN BONUS B ON A.EMPID = B.EMPID
+WHERE B.BONUS < 1000 OR B.BONUS IS NULL
+;
